d806b73f3a1648dd9bdcc3df4954d2cc890a3007
Clouds Flowing <clouds.flowing@gmail.com>
2025-12-07 17:02:03 +0800

    pretokenizer

---
+++ src/lib.rs
@@ -0,0 +1,64 @@
+use std::collections::BTreeMap;
+
+use fancy_regex::Regex;
+use lazy_static::lazy_static;
+
+lazy_static! {
+  /// PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+  static ref RE: Regex = Regex::new(r"'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+").unwrap();
+}
+
+#[derive(Debug)]
+pub struct Error {
+  pub msg: String,
+  pub loc: (&'static str, u32),
+}
+
+/// input a string and a pattern, return a map of tokens and their counts
+fn pretokenizer(s: &str, pat: &Regex) -> Result<BTreeMap<String, u64>, Error> {
+  let mut result = BTreeMap::new();
+  for i in pat.find_iter(s) {
+    match i {
+      Ok(m) => {
+        let token = m.as_str().to_string();
+        *result.entry(token).or_default() += 1;
+      }
+      Err(e) => {
+        return Err(Error {
+          msg: format!("Regex error: {}", e),
+          loc: (file!(), line!()),
+        });
+      }
+    }
+  }
+  Ok(result)
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  fn test_pretokenizer() {
+    let s = "Hello, world! It's 2024.";
+    let tokens = pretokenizer(s, &RE).unwrap();
+    let expected_tokens = vec![
+      ("Hello".to_string(), 1),
+      (",".to_string(), 1),
+      (" world".to_string(), 1),
+      ("!".to_string(), 1),
+      (" It".to_string(), 1),
+      ("'s".to_string(), 1),
+      (" 2024".to_string(), 1),
+      (".".to_string(), 1),
+    ]
+    .into_iter()
+    .collect::<BTreeMap<_, _>>();
+    assert_eq!(tokens, expected_tokens);
+  }
+
+  #[test]
+  fn test_sample() {
+    let input = std::fs::read_to_string("fixtures/tinystories_sample_5M.txt").unwrap();
+    let tokens = pretokenizer(&input, &RE).unwrap();
+  }
+}
