0cfd40de6a0eb0fee3582d9358153e71c12599a1
breezing <542538469@qq.com>
2025-12-08 04:28:28 +0800

    find chunk boundaries

--- src/pretokenizer.rs
+++ src/pretokenizer.rs
@@ -1,8 +1,12 @@
-
-use std::collections::BTreeMap;
-
 use fancy_regex::Regex;
 use lazy_static::lazy_static;
+use memchr::memmem;
+use std::{
+  collections::BTreeMap,
+  fs::{self, File},
+  io::{Read as _, Seek},
+  os::windows::fs::MetadataExt,
+};
 
 use crate::Error;
 
@@ -29,6 +33,48 @@ pub fn pretokenizer(s: &str, pat: &Regex) -> Result<BTreeMap<String, u64>, Error
     }
   }
   Ok(result)
+}
+
+pub fn find_chunk_boundaries(path: &std::path::Path, desired_num_chunks: u32, split_special_token: &str) -> Vec<u64> {
+  let file_size = fs::metadata(path).unwrap().file_size();
+  let chunk_size = file_size / desired_num_chunks as u64;
+  let mini_chunk_size = 4096;
+  let finder = memmem::Finder::new(split_special_token);
+
+  let mut boundaries = Vec::new();
+  for i in 0..(desired_num_chunks) {
+    boundaries.push(chunk_size * i as u64);
+  }
+  boundaries.push(file_size);
+
+  let mut file = File::open(path).unwrap();
+  for bi in 1..boundaries.len() - 1 {
+    let mut initial_position = boundaries[bi];
+    file.seek(std::io::SeekFrom::Start(initial_position)).unwrap();
+    loop {
+      let mut buffer = vec![0; mini_chunk_size as usize];
+      let bytes_read = file.read(&mut buffer).unwrap();
+      if bytes_read < mini_chunk_size as usize {
+        boundaries[bi] = file_size;
+        break;
+      }
+      if let Some(pos) = finder.find(buffer[..bytes_read].as_ref()) {
+        let boundary = initial_position + pos as u64;
+        boundaries[bi] = boundary;
+        break;
+      }
+      initial_position += mini_chunk_size;
+    }
+  }
+
+  let mut set = std::collections::BTreeSet::new();
+  let mut deduplicated_boundaries = boundaries
+    .iter()
+    .filter(|&item| set.insert(*item))
+    .cloned()
+    .collect::<Vec<u64>>();
+  deduplicated_boundaries.sort();
+  deduplicated_boundaries
 }
 
 #[cfg(test)]
@@ -75,5 +121,22 @@ mod tests {
     let tokens = pretokenizer(&input, &RE).unwrap();
     assert_eq!(tokens.get(" the").cloned().unwrap_or(0), 48886);
     // serde_json::to_writer_pretty(std::fs::File::create("fixtures/tinystories_sample_5M_words.json").unwrap(), &tokens).unwrap();
+  }
+
+  #[test]
+  fn test_find_chunk_boundaries() {
+    let path = std::path::Path::new("fixtures/tinystories_sample_5M.txt");
+
+    let desired_num_chunks = 4;
+    let boundaries = find_chunk_boundaries(path, desired_num_chunks, "<|endoftext|>");
+    let expect = vec![0, 1310951, 2621933, 3932548, 5242880];
+    assert!(boundaries == expect, "{:?} != {:?}", boundaries, expect);
+
+    let desired_num_chunks = 10;
+    let boundaries = find_chunk_boundaries(path, desired_num_chunks, "<|endoftext|>");
+    let expect = vec![
+      0, 525166, 1048920, 1573438, 2097691, 2621933, 3146237, 3670035, 4196392, 4718956, 5242880,
+    ];
+    assert!(boundaries == expect, "{:?} != {:?}", boundaries, expect);
   }
 }
