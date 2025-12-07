d216e8ea6aa13ad0077aa60008d222d642265d9b
Clouds Flowing <clouds.flowing@gmail.com>
2025-12-07 19:47:12 +0800

    add more test

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ pub struct Error {
 }
 
 /// input a string and a pattern, return a map of tokens and their counts
-fn pretokenizer(s: &str, pat: &Regex) -> Result<BTreeMap<String, u64>, Error> {
+pub fn pretokenizer(s: &str, pat: &Regex) -> Result<BTreeMap<String, u64>, Error> {
   let mut result = BTreeMap::new();
   for i in pat.find_iter(s) {
     match i {
@@ -54,11 +54,29 @@ mod tests {
     .into_iter()
     .collect::<BTreeMap<_, _>>();
     assert_eq!(tokens, expected_tokens);
+
+    let s = "你好，世界！Now是2024年。";
+    let tokens = pretokenizer(s, &RE).unwrap();
+    let expected_tokens = vec![
+      ("你好".to_string(), 1),
+      ("，".to_string(), 1),
+      ("世界".to_string(), 1),
+      ("！".to_string(), 1),
+      ("Now是".to_string(), 1),
+      ("2024".to_string(), 1),
+      ("年".to_string(), 1),
+      ("。".to_string(), 1),
+    ]
+    .into_iter()
+    .collect::<BTreeMap<_, _>>();
+    assert_eq!(tokens, expected_tokens);
   }
 
   #[test]
   fn test_sample() {
     let input = std::fs::read_to_string("fixtures/tinystories_sample_5M.txt").unwrap();
     let tokens = pretokenizer(&input, &RE).unwrap();
+    assert_eq!(tokens.get(" the").cloned().unwrap_or(0), 48886);
+    // serde_json::to_writer_pretty(std::fs::File::create("fixtures/tinystories_sample_5M_words.json").unwrap(), &tokens).unwrap();
   }
 }
